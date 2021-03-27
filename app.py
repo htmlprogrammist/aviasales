a9d3d09cea95b2617eb9f9e487a92a3a1100a9ca
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-27 23:51:16 +0300

    feat: improve algorithm connected with the prices, add imports to files

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-def get_price():
-    return 0
+import bot
+
+
+def get_prices():
+    return []
 
 
 def main():
     global previous_price
-    price = get_price()
+    prices = get_prices()
+
+    if len(prices) > 1:
+        price = prices[bot.clarify_the_ticket()]
+    else:
+        price = prices[0]
+
     if price < previous_price:
         print('Билеты подешевели')  # Тут будет вызов для бота...
         previous_price = price  # Перезапись цены, чтобы текущая стоимость билета для следующей была предыдущей
--- bot.py
+++ bot.py
@@ -1,4 +1,11 @@
 import telebot
 import tokens
+import app
 
 bot = telebot.TeleBot(tokens.token)
+
+
+# @handler_
+def clarify_the_ticket():
+    id_of_the_ticket = 0
+    return id_of_the_ticket
