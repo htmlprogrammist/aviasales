8b0cdc39e09f298a4dfb4228420c08e46d286f70
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-27 20:05:14 +0300

    refactor: remove useless condition

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ def get_price():
 def main():
     global previous_price
     price = get_price()
-    if previous_price == price:
-        pass
     if price < previous_price:
         print('Билеты подешевели')  # Тут будет вызов для бота...
         previous_price = price  # Перезапись цены, чтобы текущая стоимость билета для следующей была предыдущей
