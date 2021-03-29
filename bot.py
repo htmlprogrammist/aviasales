a1af363a69d950ae9aa6437bd13c2aca275caf3d
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-29 17:11:45 +0300

    feat: add repetition for main function

--- bot.py
+++ bot.py
@@ -1,8 +1,41 @@
 import telebot
 import tokens
-import app
+import datetime  # remove from the production version!
+import time
 
 bot = telebot.TeleBot(tokens.token)
+previous_price = 0
+delay = 5  # seconds
+pause = bool()  # False
+
+
+def get_prices():
+    now = datetime.datetime.now()
+    datum = now.strftime("%H:%M:%S")
+    print(datum)
+    return ['1000']  # prices
+
+
+def main():
+    global pause, previous_price
+    if not pause:
+        prices = get_prices()
+
+        if len(prices) > 1:
+            price = int(prices[clarify_the_ticket() - 1])  # Пользователь вводит номер нужного билета как ему удобно,
+            # ... а у нас, программистов, всё начинается с 0, а не с 1
+        else:  # Все остальные случаи:
+            price = int(prices[0])
+
+        if price < previous_price:
+            print('Билеты подешевели')  # Тут будет вызов для бота...
+            previous_price = price  # Перезапись цены, чтобы текущая стоимость билета для следующей была предыдущей
+        if price > previous_price:
+            print('Билеты подорожали')  # ... он будет выводит вот эту фразу и цену.
+            previous_price = price
+        time.sleep(delay)  # задержка в delay секунд
+        main()  # рекурсия, после задержки, функция вызывает саму себя и процесс повторяется
+        return previous_price
 
 
 @bot.message_handler(commands=['start'])
@@ -23,14 +56,10 @@ def help_user(message):
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text[8:21] == "travel.yandex":
-        bot.send_message(message.from_user.id, "Прекрасно! Я принял Вашу ссылку и готов работать.")
+        bot.send_message(message.from_user.id, "Прекрасно! Я принял Вашу ссылку и принимаюсь за работу.")
+        main()
     else:
-        bot.send_message(message.from_user.id, "Не понял...")
-
-
-'''
-https://travel.yandex.ru/avia/search/result/?adult_seats=1&children_seats=0&fromId=c239&infant_seats=0&klass=economy&oneway=1&return_date=&toId=c213&when=2021-04-03#empty
-'''
+        bot.send_message(message.from_user.id, "Прошу прощения, но я не понимаю, что Вы имеете в виду. Попробуйте использовать /help.")
 
 
 # @handler_
@@ -39,13 +68,8 @@ def clarify_the_ticket():  # Уточнить номер билета, на сл
     return id_of_the_ticket
 
 
-def simulate_call():
-    global pause
-    app.get_prices()
-    pause = bool(input())  # False => stops algorithm
-
-
-# pause = bool(input())
-# simulate_call()
+'''
+https://travel.yandex.ru/avia/search/result/?adult_seats=1&children_seats=0&fromId=c239&infant_seats=0&klass=economy&oneway=1&return_date=&toId=c213&when=2021-04-03#empty
+'''
 
 bot.polling(none_stop=True)
