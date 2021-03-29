bfa108f0fec543c31447a6e1eb5775fbfe44cddf
Egor Badmaev <badmaeve2511@gmail.com>
2021-03-29 16:53:49 +0300

    feat: add bot functionality, important commit

--- bot.py
+++ bot.py
@@ -5,6 +5,43 @@ import app
 bot = telebot.TeleBot(tokens.token)
 
 
+@bot.message_handler(commands=['start'])
+def get_start_message(message):
+    bot.send_message(message.from_user.id, "Здравствуйте, я Ваш авиатор. Отправьте мне ссылку на Ваш билет из Яндекс.Путешествия")
+
+
+@bot.message_handler(commands=['help'])
+def help_user(message):
+    bot.send_message(message.from_user.id,
+                     '''Если Вы не знаете, как отправить мне ссылку на билет, то следуйте следующей инструкции:
+- Откройте сайт "Яндекс.Путешествия"
+- Выберите "Авиабилеты"
+- Определите город и дату для поиска билетов, затем нажмите кнопку "Найти"
+- Вы перейдёте по ссылке, по которой и будете смотреть билеты. Скопируйте эту ссылку и отправьте мне.''')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    # if message:
+    if message.text[8:21] == "travel.yandex":
+        bot.send_message(message.from_user.id, "Прекрасно! Я принял Вашу ссылку и готов работать.")
+    else:
+        bot.send_message(message.from_user.id, "Не понял...")
+    # try:
+    #     if message.text[8:21] == "travel.yandex":
+    #         bot.send_message(message.from_user.id, "Прекрасно! Я принял Вашу ссылку и готов работать.")
+    #         # Здесь будет вызов функции, хотя возможен и вызов функции, и фидбек от бота
+    # except IndexError:
+    #     bot.send_message(message.from_user.id, "Не понял...")
+    # else:  # Не работает... куда её пихнуть - непонятно.
+    #     bot.send_message(message.from_user.id, "Прошу прощения, но я не понимаю, что Вы имеете в виду. Напишите: /help.")
+
+
+'''
+https://travel.yandex.ru/avia/search/result/?adult_seats=1&children_seats=0&fromId=c239&infant_seats=0&klass=economy&oneway=1&return_date=&toId=c213&when=2021-04-03#empty
+'''
+
+
 # @handler_
 def clarify_the_ticket():  # Уточнить номер билета, на случай, если он там не один
     id_of_the_ticket = 0
@@ -17,7 +54,7 @@ def simulate_call():
     pause = bool(input())  # False => stops algorithm
 
 
-pause = bool(input())
-simulate_call()
+# pause = bool(input())
+# simulate_call()
 
-bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True)
