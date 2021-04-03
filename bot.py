def27e2aed7015d0495c9a06c247d062d36a5a1d
Egor Badmaev <badmaeve2511@gmail.com>
2021-04-04 00:56:56 +0300

    refactor: replace parser and remove useless files
    Their history is in their own branch

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import telebot
 import config
 import threading
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 bot = telebot.TeleBot(config.token)
 previous_price = 0
@@ -10,8 +14,20 @@ id_of_the_ticket = 0
 
 
 def get_prices():
-    prices = ['1000', '1200', '1300']
-    return prices
+    chromedriver = '/Users/htmlprogrammist/Downloads/chromedriver'
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')  # для открытия headless-браузера
+    browser = webdriver.Chrome(executable_path=chromedriver, chrome_options=options)
+    browser.get("https://travel.yandex.ru/avia/search/result/?adult_seats=1&children_seats=0&fromId=c1094&infant_seats=0&klass=economy&oneway=1&return_date=&toId=c213&when=2021-04-08")
+
+    try:
+        element = WebDriverWait(browser, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "price"))
+        )
+        # print(int(element.text[:5]) + 1)  # ValueError: invalid literal for int() with base 10: '4\u2006780'
+        print(element.text[:5])
+    finally:
+        browser.quit()
 
 
 def main():
